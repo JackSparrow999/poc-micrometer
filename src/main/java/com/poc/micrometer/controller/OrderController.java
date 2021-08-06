acfde31cac11458f1f2b9940fdc6d671d7b81bd8
ronaq <ronaq.behura@acko.tech>
2021-08-06 21:51:42 +0530

    bug fix

--- src/main/java/com/poc/micrometer/controller/OrderController.java
+++ src/main/java/com/poc/micrometer/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.poc.micrometer.controller;
 
 import com.poc.micrometer.metrics.MetricHandler;
 import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class OrderController {
 
     Map<Integer, String> orders = new HashMap<>();
 
+    @Autowired
     MetricHandler metricHandler;
 
     @GetMapping("/order")
